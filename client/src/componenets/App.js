597ae524003ae8575b8ef38eea503f51018c2f30
Hiro <yassirelyakine@gmail.com>
2026-01-04 01:39:01 -0800

    @ mostly finalized the config portal's features @
    # The interface allows the user to select and highlight a particular slot for removal, which is done by the pressing of the remove slot button.
    # The interface also allows the user to append new slots, one by one, to the AO
    # which is done by the pressing of the add slot button
    # The interface allows the user to modify the watch-context and backup-context for each slot, which is done via the input fields for each in the slots
    # what's pending now is the setup of the express server to serve the react content
    # and the wiring needed to allow sufficient communication between the browser and the server for grand purpose of saving configuration to a save file (not a database format)
    # this file will then be used by the ACTUAL program to watch the precious save folders'

--- client/src/componenets/App.js
+++ client/src/componenets/App.js
@@ -5,14 +5,52 @@ import down_arrow from '../assets/down_arrow.png'
 
 function App() {
   const [ slot_active, set_slot_active ] = useState({})
-
-  const slots = [0]
+  const [ slots, set_slots ] = useState({
+    '0': { watch_context: 'watch-context', backup_context: 'backup-context', deleted: false },
+    '1': { watch_context: 'watch-context', backup_context: 'backup-context', deleted: false },
+  })
+  const [ crosshair, set_crosshair ] = useState(-1)
+  // for new slots, the correct latest ids should be calculated
+  const [ new_slot_id, set_new_slot_id ] = useState(Object.keys(slots).length)
 
 
   function toggle_slot(slot) { 
     set_slot_active({
       [slot]: !slot_active[slot]
     })
+    set_crosshair(slot)
+  }
+
+  function modify_watch_context(target, key) {
+    set_slots({
+      ...slots,
+      [key]: { ...slots[key], watch_context: target.value }
+    })
+  }
+
+  function modify_backup_context(target, key) {
+    set_slots({
+      ...slots,
+      [key]: { ...slots[key], backup_context: target.value }
+    })
+  }
+
+  function remove_slot() {
+    set_slots({
+      ...slots,
+      [crosshair]: { ...slots[crosshair], deleted: true }
+    })
+  }
+
+  function add_slot() {
+    console.log(new_slot_id)
+    set_slots({
+      ...slots,
+      [new_slot_id]: { watch_context: 'new_slot', 
+                      backup_context: 'new_slot', 
+                      deleted: false }
+    })
+    set_new_slot_id(Object.keys(slots).length + 1)
   }
 
   return (
@@ -20,24 +58,23 @@ function App() {
     <div className='AO' >
 
 
-      { slots.map( ( slot ) => (
-      <div className={`slot ${slot_active[slot] ? 'slot-active' : ''}`} onClick={ () => toggle_slot(slot) }>
-        <label style={{color: '#009900'}}>Slot {slot}</label>
-        <label>Watch this location for changes</label>
-        <input></input> {/* directory to watch */}
-        <img src={down_arrow} alt='down_arrow'></img>
-        <label>Upon changes, create new copies in this location</label>
-        <input></input> {/* backup directory */}
-      </div>
+      { Object.keys(slots).map( key => (
+        !slots[key].deleted && <div className={`slot ${slot_active[key] ? 'slot-active' : ''}`} onClick={ () => toggle_slot(key) }>
+          <label style={{color: '#009900'}}>Slot {key}</label>
+          <label>Watch this location for changes</label>
+          <input value={slots[key].watch_context} onChange={e => modify_watch_context(e.target, key)} ></input> {/* directory to watch */}
+          <img src={down_arrow} alt='down_arrow'></img>
+          <label>Upon changes, create new copies in this location</label>
+          <input value={slots[key].backup_context} onChange={e => modify_backup_context(e.target, key)} ></input> {/* backup directory */}
+        </div>
       ) ) }
-
 
     </div>
 
     <div className='buttons'>
       <button style={{backgroundColor: '#66b032'}}>Save all</button>
-      <button style={{backgroundColor: '#00bfff'}}>Add slot</button>
-      <button style={{backgroundColor: '#cc0000'}}>Remove slot</button>
+      <button style={{backgroundColor: '#00bfff'}} onClick={add_slot} >Add slot</button>
+      <button style={{backgroundColor: '#cc0000'}} onClick={remove_slot}>Remove slot</button>
     </div>
 
     </>
