d9d50414e0791bbb7b0f2db906e7027f19214819
Hiro <yassirelyakine@gmail.com>
2026-01-18 00:22:58 -0800

    I wrote a preliminary proof of concept infinite loop which requests os to signal change handle next time there is an appropriate change

--- watcher/main.cpp
+++ watcher/main.cpp
@@ -3,51 +3,71 @@
 #include <windows.h>
 using namespace std;
 
-#define DEV_PATH_NUMBER 2
+#define DEV_PATH_NUMBER 7
 
-#define SUCCESS 0
 #define PROGRAM_FAILED 1
+
+#define GAME_SAVING_WAIT_TIME 5000
 
 int main() 
 {
-    const char *path[DEV_PATH_NUMBER] = ["/c/Users/Administrator/Desktop/Helios/watcher/test0", "/c/Users/Administrator/Desktop/Helios/watcher/test1"];
+    const char *path[DEV_PATH_NUMBER] = {"", "", "", "/c/Users/Administrator/Desktop/Helios/watcher/test1", "", "/fdfdfd/", "/fdf"};
     wchar_t wideCharPath[MAX_PATH];
     size_t mbstowcsReturn;
     HANDLE handles[DEV_PATH_NUMBER];
+    int realNumberOfHandles = 0;
 
     cout << "creating change handles...\n";
     for ( int i = 0, j = 0; i < DEV_PATH_NUMBER; i++ ) 
     {
-        if ( mbstowcs_s(&mbstowcsReturn, wideCharPath, MAX_PATH, path[i], MAX_PATH) != SUCCESS )
+        cout << "handle at index " << j << " [\"" << path[i] << "\"]";
+        /* FindFirstChangeNotification() requires a wchar_t for some reason so this function fills wideCharPath from path[i] */
+        if ( mbstowcs_s(&mbstowcsReturn, wideCharPath, MAX_PATH, path[i], MAX_PATH) != 0 )
         {
-            cerr << "mbstowcs_s() failed\n";
+            cerr << "\nmbstowcs_s() failed\n";
             return PROGRAM_FAILED;
         }
-        if ( mbstowcsReturn == 0 )
+        /* store the non-ignored change handles inside the array */
+        handles[j] = FindFirstChangeNotification((const char *)wideCharPath, TRUE, FILE_NOTIFY_CHANGE_SIZE);
+        if ( handles[j] == INVALID_HANDLE_VALUE )
         {
-            cout << "empty watch-context path, ignored.\n";
+            cerr << "\ninvalid handle, ignored.\n";
             continue;
         }
-        handle[j] = FindFirstChangeNotification((const char *)wideCharPath, TRUE, FILE_NOTIFY_CHANGE_SIZE);
-        if ( handle[j] == INVALID_HANDLE_VALUE )
-        {
-            cout << "invalid handle, ignored.\n";
-            continue;
-        }
+        cout << " created.\n";
         j++;
+        realNumberOfHandles++;
 
     }
 
-    HANDLE handle = FindFirstChangeNotification((const char *)wideCharPath, TRUE,
-                                                FILE_NOTIFY_CHANGE_SIZE);
+    while ( true )
+        for ( int i = 0; i < realNumberOfHandles; i++ )
+        {
+            DWORD status = WaitForSingleObject(handles[i], 0);
 
-    if ( handle ==  INVALID_HANDLE_VALUE )
-        return 1;
-
-    const DWORD waitstatus = WaitForMultipleObjects(1, &handle, TRUE, 10000);
-
-    if ( waitstatus == WAIT_OBJECT_0 )
-        cout << "directory changed!";
-
+            if ( status == WAIT_OBJECT_0 )
+            {
+                cout << "write detected for handle at index " << i << "\n";
+                if ( FindNextChangeNotification(handles[i]) == 0 )
+                {
+                    cerr << "batch FindNextChangeNotification() failed for handle at index " << i << "\n";
+                    continue;
+                }
+                /* to avoid writing multiple incomplete backups, we "batch" signals instead */
+                status = WaitForSingleObject(handles[i], GAME_SAVING_WAIT_TIME);
+                // write backups code here
+                if ( FindNextChangeNotification(handles[i]) == 0 )
+                {
+                    cerr << "bridge findNextChangeNotification() failed for handle at index " << i << "\n";
+                    continue;
+                }
+                if ( status == WAIT_OBJECT_0 )
+                    cout << "delayed writes detected for handle at index " << i << "\n";
+                else if ( status == WAIT_FAILED )
+                    cerr << "nested WAIT_FAILED for handle at index " << i << "\n";
+            }
+            else if ( status == WAIT_FAILED )
+                cerr << "WAIT_FAILED for handle at index " << i << "\n";
+        }
     
 }
