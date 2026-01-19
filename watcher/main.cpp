f497a4b9c902af28c576ebce4fa1ca5cc5e8214c
Hiro <yassirelyakine@gmail.com>
2026-01-19 00:45:48 -0800

    it turns out that FindFirstChangeNotification() and ReadDirectoryChangesW() are mutually exclusive, and as a result I had to change the initialization steps, now the function CreateFile() is used to open a directory and return a handle to said directory, that handle is used by ReadDirectoryChangesW()
    WaitForSingleObject() can still be used to wait on the hEvent handle stored in the OVERLAPPED object which is passed to ReadDirectoryChangesW() function, any notifications are detected through waiting on such object.

--- watcher/main.cpp
+++ watcher/main.cpp
@@ -1,31 +1,22 @@
 
-#include <iostream>
-#include <windows.h>
-#include <filesystem>
+#include "watcher.hpp"
 using namespace std;
-
-
-#define DEV_PATH_NUMBER 7
-
-#define PROGRAM_FAILED 1
-
-#define GAME_SAVING_WAIT_TIME 5000
 
 int main() 
 {
     const char *path[DEV_PATH_NUMBER] = {"", "", "", "\\Users\\Administrator\\Desktop\\cv\\", "", "/fdfdfd/", "/fdf"};
     wchar_t wideCharPath[MAX_PATH];
     size_t mbstowcsReturn;
-    HANDLE handles[DEV_PATH_NUMBER];
-    int realNumberOfHandles = 0;
+    WATCH_CONTEXT context[DEV_PATH_NUMBER];
+    int realNumberOfcontext = 0;
 
-    cout << "creating change handles...\n";
+    cout << "initializing monitoring tools...\n";
     for ( int i = 0, j = 0; i < DEV_PATH_NUMBER; i++ ) 
     {
-        cout << "handle at index " << j << " [\"" << path[i] << "\"]";
+        cout << "path " << " [\"" << path[i] << "\"]";
         if ( !filesystem::exists(path[i]) )
         {
-            cerr << "\npath " << path[i] << " does not exist, ignored\n";
+            cerr << " does not exist, ignored\n";
             continue;
         }
         /* FindFirstChangeNotification() requires a wchar_t for some reason so this function fills wideCharPath from path[i] */
@@ -34,49 +25,68 @@ int main()
             cerr << "\nmbstowcs_s() failed\n";
             return PROGRAM_FAILED;
         }
-        /* store the non-ignored change handles inside the array */
-        handles[j] = FindFirstChangeNotification((const char *)wideCharPath, TRUE, FILE_NOTIFY_CHANGE_SIZE | 
-                                                    FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_LAST_WRITE |
-                                                 FILE_NOTIFY_CHANGE_FILE_NAME );
-        if ( handles[j] == INVALID_HANDLE_VALUE )
+        context[j].handle = CreateFile(
+            path[i],
+            FILE_LIST_DIRECTORY,
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+            NULL,
+            OPEN_EXISTING,
+            FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
+            NULL
+        );
+        if ( context[j].handle == INVALID_HANDLE_VALUE )
         {
             cerr << "\ninvalid handle, ignored.\n";
             continue;
         }
-        cout << " created.\n";
+        /* this object is used to supply the event handle to ReadDirectoryChangesW() 
+        which we'll later wait on for signals */
+        context[j].overlapped.hEvent = CreateEvent(NULL, FALSE, 0, NULL);
+        /* the change buffer is where change information is stored
+        (we do that ourselves via another windows function ) */
+        // uint8_t changeBuf[CHANGE_BUFFER_SIZE];
+        BOOL success = ReadDirectoryChangesW(
+            context[j].handle, context[j].changeBuf, CHANGE_BUFFER_SIZE, TRUE,
+            FILE_NOTIFY_CHANGE_FILE_NAME  | 
+            FILE_NOTIFY_CHANGE_DIR_NAME   |
+            FILE_NOTIFY_CHANGE_LAST_WRITE,
+            NULL, &context[j].overlapped, NULL
+        );
+        cout << " assimilated.\n";
         j++;
-        realNumberOfHandles++;
+        realNumberOfcontext++;
 
     }
 
-    while ( true )
-        for ( int i = 0; i < realNumberOfHandles; i++ )
-        {
-            DWORD status = WaitForSingleObject(handles[i], 0);
 
-            if ( status == WAIT_OBJECT_0 )
-            {
-                cout << "write detected for handle at index " << i << "\n";
-                if ( FindNextChangeNotification(handles[i]) == 0 )
-                {
-                    cerr << "batch FindNextChangeNotification() failed for handle at index " << i << "\n";
-                    continue;
-                }
-                /* to avoid writing multiple incomplete backups, we "batch" signals instead */
-                status = WaitForSingleObject(handles[i], GAME_SAVING_WAIT_TIME);
-                // write backups code here
-                if ( FindNextChangeNotification(handles[i]) == 0 )
-                {
-                    cerr << "bridge findNextChangeNotification() failed for handle at index " << i << "\n";
-                    continue;
-                }
-                if ( status == WAIT_OBJECT_0 )
-                    cout << "delayed writes detected for handle at index " << i << "\n";
-                else if ( status == WAIT_FAILED )
-                    cerr << "nested WAIT_FAILED for handle at index " << i << "\n";
-            }
-            else if ( status == WAIT_FAILED )
-                cerr << "WAIT_FAILED for handle at index " << i << "\n";
-        }
+    // while ( true )
+    //     for ( int i = 0; i < realNumberOfcontext; i++ )
+    //     {
+    //         DWORD status = WaitForSingleObject(context[i], 0);
+
+    //         if ( status == WAIT_OBJECT_0 )
+    //         {
+    //             cout << "write detected for handle at index " << i << "\n";
+    //             if ( FindNextChangeNotification(context[i]) == 0 )
+    //             {
+    //                 cerr << "batch FindNextChangeNotification() failed for handle at index " << i << "\n";
+    //                 continue;
+    //             }
+    //             /* to avoid writing multiple incomplete backups, we "batch" signals instead */
+    //             status = WaitForSingleObject(context[i], GAME_SAVING_WAIT_TIME);
+    //             // write backups code here
+    //             if ( FindNextChangeNotification(context[i]) == 0 )
+    //             {
+    //                 cerr << "bridge findNextChangeNotification() failed for handle at index " << i << "\n";
+    //                 continue;
+    //             }
+    //             if ( status == WAIT_OBJECT_0 )
+    //                 cout << "delayed writes detected for handle at index " << i << "\n";
+    //             else if ( status == WAIT_FAILED )
+    //                 cerr << "nested WAIT_FAILED for handle at index " << i << "\n";
+    //         }
+    //         else if ( status == WAIT_FAILED )
+    //             cerr << "WAIT_FAILED for handle at index " << i << "\n";
+    //     }
     
 }
---
+++ watcher/watcher.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <filesystem>
+
+#define DEV_PATH_NUMBER 7
+#define PROGRAM_FAILED 1
+#define GAME_SAVING_WAIT_TIME 5000
+#define CHANGE_BUFFER_SIZE 1024
+
+typedef struct {
+    HANDLE handle;
+    OVERLAPPED overlapped;
+    uint8_t changeBuf[CHANGE_BUFFER_SIZE];
+} WATCH_CONTEXT;
